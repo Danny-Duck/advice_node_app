0037d8f1fa83585d263663b98d5f2a33d7c68772
Danny_knows <dannylow456@gmail.com>
2020-06-04 15:07:03 +1000

    added welcome logic and package dependancies

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+const chalk = require("chalk");
+const readline = require("readline-sync");
+
+console.clear();
+console.log("hello there");
+
+console.log("what's your name?");
+
+const name = readline.question("> ");
+
+console.clear()
+
+const wantAdvice = readline.keyInYN([[`hi ${name}, would you like some advice?`]])
+
+if (wantAdvice === false){
+  console.log('ok have a nice day :)');
+  process.exit()
+} 
+
+console.log('youre in luck !')
