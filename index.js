01c01c69110e11e8ba978343f9764236e904c5eb
Danny_knows <dannylow456@gmail.com>
2020-06-05 11:25:37 +1000

    refactored getAdvice function, added some delish grammer regex

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const chalk = require("chalk");
 const readline = require("readline-sync");
 const fetch = require("node-fetch");
 
-const colors = ['red', 'blue', 'green', 'magenta']
-
-// const p = (string) => {
-//   console.log(string);
-// };
-
+const colors = ["red", "blue", "green", "magenta"];
+const errMessage = "sorry grandfather doesn't want to give advice right now";
 console.clear();
 console.log("hello there");
 
@@ -17,31 +13,32 @@ const name = readline.question("> ");
 
 console.clear();
 
-const wantAdvice = readline.keyInYN([[`hi ${name}, would you like some advice?`]]);
+const wantAdvice = readline.keyInYN([[`hi ${name}, would you like some grandfather advice?`]]);
 
 if (wantAdvice === false) {
   console.log("ok have a nice day :)");
   process.exit();
 }
 
-console.log("youre in luck !");
 const getAdvice = async () => {
   try {
-    const response = await fetch("https://api.adviceslip.com/advice")
-    if (!response.ok){
-      throw new Error('Error with response')
+    const response = await fetch("https://api.adviceslip.com/advice");
+    if (!response.ok) {
+      throw new Error(errMessage);
     }
-    const data = await response.json()
-    const result = data.slip.advice
-    console.log("whats your favorite color?");
-    const index = readline.keyInSelect(colors, 'Choose the color number');  
-    console.log(chalk[colors[index]](result))
-  }catch(err){
-    console.log("sorry grandfather doesn't want to give advice right now");
+    const data = await response.json();
+    const result = data.slip.advice;
+    colorPick(result.replace(/\./, "!"));
+  } catch (err) {
+    console.log(errMessage);
   }
-}
+};
 
-getAdvice()
+const colorPick = (result) => {
+  console.log("whats your favorite color?");
+  const index = readline.keyInSelect(colors, "Choose the color number");
+  console.clear();
+  console.log(`grandfather says "${chalk[colors[index]](result)}"`);
+};
 
-// if promise works, print advice in favorite color
-// if not, say sorry pal grandfather says no
+getAdvice();
